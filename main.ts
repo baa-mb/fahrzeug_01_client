78ee4e252e930257b12cd32e7a691cf7ad2a539f
baa <34665120+baa-mb@users.noreply.github.com>
2021-09-29 19:41:53 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,22 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString("A")
+    lauf = 1
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendString("AB")
+    lauf = 1
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendString("B")
+    lauf = 0
 })
+let lauf = 0
 basic.showIcon(IconNames.SmallDiamond)
 radio.setGroup(54)
+lauf = 0
+basic.forever(function () {
+    if (lauf == 1) {
+        radio.sendString(convertToText(input.acceleration(Dimension.X)))
+        basic.pause(200)
+    }
+})
